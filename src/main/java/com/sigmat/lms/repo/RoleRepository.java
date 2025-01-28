d335d3c2a5dfaf960c269d7ab843bd54e26deb1f
VishaL6i9 <vishalkandakatla@gmail.com>
2025-01-29 03:28:59 +0530

    Added Repo for user roles

---
+++ src/main/java/com/sigmat/lms/repo/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.sigmat.lms.repo;
+
+import com.sigmat.lms.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
