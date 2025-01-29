c94d4b42d495bfb66e7171e0c093d0e7548d9be3
VishaL6i9 <vishalkandakatla@gmail.com>
2025-01-30 02:57:52 +0530

    Fixed Role Based Access Auth bug where new users were stripped of role after api call.

--- src/main/java/com/sigmat/lms/services/UserDetailService.java
+++ src/main/java/com/sigmat/lms/services/UserDetailService.java
@@ -1,25 +1,48 @@
 package com.sigmat.lms.services;
 
+import com.sigmat.lms.models.Role;
 import com.sigmat.lms.models.UserPrincipal;
 import com.sigmat.lms.models.Users;
 import com.sigmat.lms.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.Set;
+
 @Service
 public class UserDetailService implements UserDetailsService {
+
     @Autowired
     private UserRepo userRepo;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Users user = userRepo.findByUsername(username);
-       if(user == null) {
-            System.out.println("User not found");
+        Users user = userRepo.findByUsername(username); 
+
+        if (user == null) {
             throw new UsernameNotFoundException("User not found");
         }
-        return new UserPrincipal(user);
+
+        Set<Role> roles = user.getRoles();
+        if (roles == null || roles.isEmpty()) {
+            throw new UsernameNotFoundException("User has no roles assigned");
+        }
+
+        Collection<GrantedAuthority> authorities = roles.stream()
+                .flatMap(role -> {
+                    Collection<GrantedAuthority> roleAuthorities = role.getAuthorities();
+                    roleAuthorities.add(new SimpleGrantedAuthority(role.getRoleName()));
+                    return roleAuthorities.stream();
+                })
+                .toList();
+
+        return new UserPrincipal(user
+        );
     }
 }
