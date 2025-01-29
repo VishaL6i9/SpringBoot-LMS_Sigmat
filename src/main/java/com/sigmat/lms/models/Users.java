0f0815bcdc4ceadc04498d33d57e233d11512761
VishaL6i9 <vishalkandakatla@gmail.com>
2025-01-30 02:55:50 +0530

    Updated Model Users

--- src/main/java/com/sigmat/lms/models/Users.java
+++ src/main/java/com/sigmat/lms/models/Users.java
@@ -1,28 +1,35 @@
 package com.sigmat.lms.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Users {
-    public Users(int id,String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-
-    }
     @Id
-    private int id;
-    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Long id; 
+
+    @Column(nullable = false, unique = true) 
     private String username;
+
+    @Column(nullable = false) 
     private String password;
 
-    public Users() {
-        
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER) 
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles = new HashSet<>();
+
+    public Users(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
 }
