ff42168d3cecbd389b8f31f829c5c8cc546c45ba
VishaL6i9 <vishalkandakatla@gmail.com>
2025-01-30 03:04:22 +0530

    Added Static Role Authorities for all types of api call

---
+++ src/main/java/com/sigmat/lms/models/Permission.java
@@ -0,0 +1,23 @@
+package com.sigmat.lms.models;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum Permission {
+    ADMIN_READ("admin:read"),
+    ADMIN_CREATE("admin:create"),
+    ADMIN_UPDATE("admin:update"),
+    ADMIN_DELETE("admin:delete"),
+    MANAGER_READ("manager:read"),
+    MANAGER_CREATE("manager:create"),
+    MANAGER_UPDATE("manager:update"),
+    MANAGER_DELETE("manager:delete");
+
+    private final String permission;
+
+    public String getAuthority() {
+        return this.permission;
+    }
+}
