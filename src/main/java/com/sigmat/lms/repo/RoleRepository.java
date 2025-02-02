c8b0479da31fd7e7ac41765c53775c52e20f3e52
VishaL6i9 <vishalkandakatla@gmail.com>
2025-02-03 00:19:38 +0530

    Updated Dependencies

--- src/main/java/com/sigmat/lms/repo/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sigmat.lms.repo;
-
-import com.sigmat.lms.models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
