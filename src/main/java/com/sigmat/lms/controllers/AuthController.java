fb66f58b04ff829357d8b7a49a72fe9c35cb9c72
VishaL6i9 <vishalkandakatla@gmail.com>
2025-02-07 02:18:19 +0530

    Updated Public Routing

--- src/main/java/com/sigmat/lms/controllers/AuthController.java
+++ src/main/java/com/sigmat/lms/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package com.sigmat.lms.controllers;
 
+import com.sigmat.lms.models.Role;
 import com.sigmat.lms.models.Users;
 import com.sigmat.lms.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,7 @@ public class AuthController {
     public ResponseEntity<?> login(@RequestBody Users loginUser ) {
         String username = loginUser .getUsername();
         String password = loginUser .getPassword();
-        
+
         if (userService.validateUser (username, password)) {
             String token = userService.generateToken(username);
             return ResponseEntity.ok().body("Login successful! Token: " + token);
@@ -26,15 +27,25 @@ public class AuthController {
         }
     }
 
-    @PostMapping("/register")
+    @PostMapping("/register/user")
     public ResponseEntity<?> register(@RequestBody Users newUser ) {
-        userService.saveUser (newUser );
-        return ResponseEntity.ok().body("User  registered successfully!");
+        Users userToSave = new Users();
+        userToSave.setId(newUser.getId());
+        userToSave.setUsername(newUser .getUsername());
+        userToSave.setPassword(newUser .getPassword());
+        userToSave.getRoles().add(Role.LEARNER); 
+
+        try {
+            userService.saveUser (userToSave);
+            return ResponseEntity.ok().body("User  registered successfully!");
+        } catch (Exception e) {
+            return ResponseEntity.status(400).body("Registration failed: " + e.getMessage());
+        }
     }
+    
+
     @GetMapping("/test")
     public ResponseEntity<?> test() {
         return ResponseEntity.ok().body("Test successful");
     }
-    
-    
 }
