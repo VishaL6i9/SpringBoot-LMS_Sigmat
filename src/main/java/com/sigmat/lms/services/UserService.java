bd8b42b0d172746030bb26e5069773268aa5fe1f
VishaL6i9 <vishalkandakatla@gmail.com>
2025-02-07 10:54:29 +0530

    Updated User Service To Catch Transaction Lock

--- src/main/java/com/sigmat/lms/services/UserService.java
+++ src/main/java/com/sigmat/lms/services/UserService.java
@@ -3,8 +3,10 @@ package com.sigmat.lms.services;
 import com.sigmat.lms.models.Users;
 import com.sigmat.lms.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
@@ -16,7 +18,7 @@ public class UserService {
     @Autowired
     private JwtService jwtUtil;
 
-    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public boolean validateUser (String username, String password) {
         Users user = userRepository.findByUsername(username);
@@ -26,13 +28,17 @@ public class UserService {
     public String generateToken(String username) {
         return jwtUtil.generateToken(username);
     }
-
+    
     public void saveUser (Users user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword())); 
-        userRepository.save(user);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        try {
+            userRepository.save(user);
+        } catch (ObjectOptimisticLockingFailureException e) {
+            throw new RuntimeException("The user was updated by another transaction. Please try again.");
+        }
     }
 
     public List<Users> getAllUsers() {
-        return userRepository.findAll(); 
+        return userRepository.findAll();
     }
 }
