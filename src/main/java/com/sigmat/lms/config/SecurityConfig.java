9729984a2e436e321ebfb16508b0237816f1cc68
VishaL6i9 <vishalkandakatla@gmail.com>
2025-02-11 01:26:08 +0530

    Added More Addresses Into CORS

--- src/main/java/com/sigmat/lms/config/SecurityConfig.java
+++ src/main/java/com/sigmat/lms/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); 
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","http://localhost:3001")); 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); 
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); 
