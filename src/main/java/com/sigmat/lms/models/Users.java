69532d1a279f2e5d422ce0f58a7c52393dc1f048
VishaL6i9 <vishalkandakatla@gmail.com>
2025-02-13 01:32:27 +0530

    Updated User Model To Support UserProfile Dynamic Generation Properly

--- src/main/java/com/sigmat/lms/models/Users.java
+++ src/main/java/com/sigmat/lms/models/Users.java
@@ -1,13 +1,13 @@
 package com.sigmat.lms.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "users")
@@ -16,22 +16,28 @@ public class Users {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String username;
-
+    
     @Column(nullable = false)
     private String password;
+    
+    @Column(unique = true)
+    private String email;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
 
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
     @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
     private Set<Role> roles = new HashSet<>();
 
+    @JsonIgnore
     @OneToOne(mappedBy = "users", cascade = CascadeType.ALL, fetch = FetchType.LAZY, optional = false)
     private UserProfile userProfile;
-
-
-
+    
     @PrePersist
     public void prePersist() {
         if (userProfile == null) {
