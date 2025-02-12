70910fa85779d469b033846081d4b00363a7c88d
VishaL6i9 <vishalkandakatla@gmail.com>
2025-02-13 01:36:33 +0530

    Updated Routes To Use UserDTO For All Logic

--- src/main/java/com/sigmat/lms/controllers/AuthController.java
+++ src/main/java/com/sigmat/lms/controllers/AuthController.java
@@ -6,6 +6,9 @@ import com.sigmat.lms.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.logging.Logger;
+import com.sigmat.lms.models.UserDTO;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/public")
@@ -13,41 +16,64 @@ public class AuthController {
 
     @Autowired
     private UserService userService;
-
+    private static final Logger LOGGER = Logger.getLogger(AuthController.class.getName());
+    
+    
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody Users loginUser ) {
+
+    public ResponseEntity<?> login(@RequestBody UserDTO loginUser ) {
+
         String username = loginUser .getUsername();
+
         String password = loginUser .getPassword();
 
-        if (userService.validateUser (username, password)) {
-            String token = userService.generateToken(username);
-            return ResponseEntity.ok().body(token);
-        } else {
-            return ResponseEntity.status(401).body("Login failed! Invalid credentials.");
-        }
-    }
 
+        if (userService.validateUser (username, password)) {
+
+            String token = userService.generateToken(username);
+
+            return ResponseEntity.ok().body(token);
+
+        } else {
+
+            return ResponseEntity.status(401).body(Map.of("message", "Login failed! Invalid credentials."));
+
+        }
+
+    }
+    
     @PostMapping("/register/user")
     public ResponseEntity<?> register(@RequestBody Users newUser ) {
+        
         Users userToSave = new Users();
-        userToSave.setId(newUser.getId());
+        
         userToSave.setUsername(newUser .getUsername());
-        userToSave.setPassword(newUser .getPassword());
-        userToSave.getRoles().add(Role.LEARNER);
-
+        userToSave.setPassword(newUser .getPassword()); 
+        userToSave.setEmail(newUser .getEmail());
+        userToSave.setFirstName(newUser .getFirstName());
+        userToSave.setLastName(newUser .getLastName());
+    
+        // Clear existing roles and add new roles from the input
+        userToSave.getRoles().clear();
+        if (newUser .getRoles() != null && !newUser .getRoles().isEmpty()) {
+            userToSave.getRoles().addAll(newUser .getRoles());
+        } else {
+            userToSave.getRoles().add(Role.LEARNER); 
+        }
+    
         try {
             userService.saveUser (userToSave);
             return ResponseEntity.ok().body("User  registered successfully!");
         } catch (Exception e) {
+            
+            LOGGER.severe("Registration failed: " + e.getMessage());
             return ResponseEntity.status(400).body("Registration failed: " + e.getMessage());
         }
     }
-
 
     @GetMapping("/test")
     public ResponseEntity<?> test() {
         return ResponseEntity.ok().body("Test successful");
     }
-    
     
 }
