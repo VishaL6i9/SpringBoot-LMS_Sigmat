fe76ca8b5b492b6eb7a0f1f6fd49cb11222f7c27
VishaL6i9 <vishalkandakatla@gmail.com>
2025-02-13 01:35:48 +0530

    Added UserDTO Model

---
+++ src/main/java/com/sigmat/lms/models/UserDTO.java
@@ -0,0 +1,13 @@
+package com.sigmat.lms.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private String username;
+    private String password;
+}
