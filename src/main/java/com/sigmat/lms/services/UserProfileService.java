b88a01215cbbb4431773b7f14dbeb0ac30c5f5d0
VishaL6i9 <vishalkandakatla@gmail.com>
2025-02-25 21:18:37 +0530

    Replaced Logic To Update User Profile

--- src/main/java/com/sigmat/lms/services/UserProfileService.java
+++ src/main/java/com/sigmat/lms/services/UserProfileService.java
@@ -3,19 +3,42 @@ package com.sigmat.lms.services;
 import com.sigmat.lms.models.UserProfile;
 import com.sigmat.lms.repo.UserProfileRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserProfileService {
 
     @Autowired
     private UserProfileRepo userProfileRepository;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public UserProfile getUserProfile(Long userId) {
         return userProfileRepository.findByUsersId(userId);
     }
 
     public UserProfile updateUserProfile(UserProfile userProfile) {
-        return userProfileRepository.save(userProfile);
+        
+        Optional<UserProfile> existingProfile = userProfileRepository.findById(userProfile.getId());
+
+        if (existingProfile.isPresent()) {
+            UserProfile updatedProfile = existingProfile.get();
+            updatedProfile.setFirstName(userProfile.getFirstName());
+            updatedProfile.setLastName(userProfile.getLastName());
+            updatedProfile.setEmail(userProfile.getEmail());
+            updatedProfile.setPhone(userProfile.getPhone());
+            updatedProfile.setTimezone(userProfile.getTimezone());
+            updatedProfile.setLanguage(userProfile.getLanguage());
+            updatedProfile.setProfileImage(userProfile.getProfileImage());
+
+            if (userProfile.getPassword() != null && !userProfile.getPassword().isEmpty()) {
+                updatedProfile.setPassword(passwordEncoder.encode(userProfile.getPassword())); // Hash the password before saving
+            }
+            return userProfileRepository.save(updatedProfile);
+        } else {
+            throw new RuntimeException("User  Profile not found with id: " + userProfile.getId());
+        }
     }
 }
