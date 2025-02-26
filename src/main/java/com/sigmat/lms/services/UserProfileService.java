9f2b727fa3840a616016bd3b427c43ba961856ba
VishaL6i9 <vishalkandakatla@gmail.com>
2025-02-26 20:59:16 +0530

    Added Logic To Update User Based On Updated User Profile

--- src/main/java/com/sigmat/lms/services/UserProfileService.java
+++ src/main/java/com/sigmat/lms/services/UserProfileService.java
@@ -1,11 +1,12 @@
 package com.sigmat.lms.services;
 
 import com.sigmat.lms.models.UserProfile;
+import com.sigmat.lms.models.Users;
 import com.sigmat.lms.repo.UserProfileRepo;
+import com.sigmat.lms.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.util.Optional;
 
 @Service
@@ -13,6 +14,8 @@ public class UserProfileService {
 
     @Autowired
     private UserProfileRepo userProfileRepository;
+    @Autowired
+    private UserRepo userRepo;
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public UserProfile getUserProfile(Long userId) {
@@ -21,10 +24,13 @@ public class UserProfileService {
 
     public UserProfile updateUserProfile(UserProfile userProfile) {
         
-        Optional<UserProfile> existingProfile = userProfileRepository.findById(userProfile.getId());
+        Optional<UserProfile> existingProfile = Optional.ofNullable(userProfileRepository.findByUsersId(userProfile.getId()));
+        Optional<Users> existingUser = Optional.ofNullable(userRepo.findById(userProfile.getId()));
 
         if (existingProfile.isPresent()) {
             UserProfile updatedProfile = existingProfile.get();
+            Users updatedUser = existingUser.get();
+            
             updatedProfile.setFirstName(userProfile.getFirstName());
             updatedProfile.setLastName(userProfile.getLastName());
             updatedProfile.setEmail(userProfile.getEmail());
@@ -32,13 +38,20 @@ public class UserProfileService {
             updatedProfile.setTimezone(userProfile.getTimezone());
             updatedProfile.setLanguage(userProfile.getLanguage());
             updatedProfile.setProfileImage(userProfile.getProfileImage());
+            
+            updatedUser.setFirstName(userProfile.getFirstName());
+            updatedUser.setLastName(userProfile.getLastName());
+            updatedUser.setEmail(userProfile.getEmail());
+            
+            
 
             if (userProfile.getPassword() != null && !userProfile.getPassword().isEmpty()) {
-                updatedProfile.setPassword(passwordEncoder.encode(userProfile.getPassword())); // Hash the password before saving
+                updatedProfile.setPassword(passwordEncoder.encode(userProfile.getPassword()));
+                updatedUser.setPassword(passwordEncoder.encode(userProfile.getPassword()));
             }
             return userProfileRepository.save(updatedProfile);
         } else {
-            throw new RuntimeException("User  Profile not found with id: " + userProfile.getId());
+            throw new RuntimeException("User Profile not found with id: " + userProfile.getId());
         }
     }
 }
