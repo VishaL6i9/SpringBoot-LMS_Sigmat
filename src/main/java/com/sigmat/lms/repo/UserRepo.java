e32e029aaa210ee31b92367bdbaf5bd17f2e5d0d
VishaL6i9 <vishalkandakatla@gmail.com>
2025-03-02 21:48:15 +0530

    Added Optional Handling to findByID method

--- src/main/java/com/sigmat/lms/repo/UserRepo.java
+++ src/main/java/com/sigmat/lms/repo/UserRepo.java
@@ -3,7 +3,9 @@ package com.sigmat.lms.repo;
 import com.sigmat.lms.models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<Users, Integer> {
     Users findByUsername(String username);
-    Users findById(Long id);
+    Optional<Users> findById(Long id);
 }
