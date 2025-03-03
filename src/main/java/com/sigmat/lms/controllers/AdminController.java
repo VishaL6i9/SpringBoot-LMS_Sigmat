780bb8c049a60d26dec35f45460c9794b63776bb
VishaL6i9 <vishalkandakatla@gmail.com>
2025-03-03 19:06:12 +0530

    Added Endpoints - Batch Delete

--- src/main/java/com/sigmat/lms/controllers/AdminController.java
+++ src/main/java/com/sigmat/lms/controllers/AdminController.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+
 @RestController
 @RequestMapping("/api/admin")
 @PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
+
     @Autowired
     private UserService userService;
 
@@ -20,13 +23,34 @@ public class AdminController {
         List<Users> users = userService.getAllUsers();
         return ResponseEntity.ok(users);
     }
+
     @DeleteMapping("/delete/user/{username}")
-    public ResponseEntity<?> deleteUser(@PathVariable String username) {
+    public ResponseEntity<?> deleteUser (@PathVariable String username) {
         try {
             userService.deleteUserByUsername(username);
-            return ResponseEntity.ok().body("User deleted successfully!");
+            return ResponseEntity.ok().body("User  deleted successfully!");
         } catch (Exception e) {
-            return ResponseEntity.status(404).body("User not found or deletion failed: " + e.getMessage());
+            return ResponseEntity.status(404).body("User  not found or deletion failed: " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/delete/users/batch")
+    public ResponseEntity<?> deleteUsersBatch(@RequestParam("file") MultipartFile file) {
+        List<String> usernames;
+        try {
+            if (file.getOriginalFilename().endsWith(".csv")) {
+                usernames = userService.readUsernamesFromCSV(file);
+            } else if (file.getOriginalFilename().endsWith(".xlsx") || file.getOriginalFilename().endsWith(".xls")) {
+                usernames = userService.readUsernamesFromExcel(file);
+            } else {
+                return ResponseEntity.badRequest().body("Invalid file type. Please upload a CSV or Excel file.");
+            }
+            for (String username : usernames) {
+                userService.deleteUserByUsername(username);
+            }
+            return ResponseEntity.ok().body("Users deleted successfully!");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error occurred during deletion: " + e.getMessage());
         }
     }
 }
