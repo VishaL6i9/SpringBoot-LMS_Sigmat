4cb74c79a976967c803555551e5c80a54fc96507
VishaL6i9 <vishalkandakatla@gmail.com>
2025-03-11 21:57:58 +0530

    Refactored Code

--- src/main/java/com/sigmat/lms/controllers/AuthController.java
+++ src/main/java/com/sigmat/lms/controllers/AuthController.java
@@ -6,12 +6,10 @@ import com.sigmat.lms.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.logging.Logger;
 import com.sigmat.lms.models.UserDTO;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.util.Map;
 
 @RestController
