a677487b1401711e3781731461b81ede3d362b85
VishaL6i9 <vishalkandakatla@gmail.com>
2025-03-23 16:59:32 +0530

    Added Custom UserProfileNotFoundException

---
+++ src/main/java/com/sigmat/lms/exceptions/UserProfileNotFoundException.java
@@ -0,0 +1,11 @@
+package com.sigmat.lms.exceptions;
+
+public class UserProfileNotFoundException extends RuntimeException {
+    public UserProfileNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserProfileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
