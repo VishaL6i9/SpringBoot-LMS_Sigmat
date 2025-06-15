fe976ddf74f330442fa39d4f0fc0d2b2fa389658
VishaL6i9 <vishalkandakatla@gmail.com>
2025-06-15 18:44:53 +0530

    Removed <Optional> wrap in findById method

--- src/main/java/com/sigmat/lms/repo/UserRepo.java
+++ src/main/java/com/sigmat/lms/repo/UserRepo.java
@@ -2,11 +2,9 @@ package com.sigmat.lms.repo;
 
 import com.sigmat.lms.models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface UserRepo extends JpaRepository<Users, Integer> {
     Users findByUsername(String username);
-    Optional<Users> findById(Long id);
-
+    Users findById(Long id);
     Users findByVerificationToken(String verificationToken);
 }
