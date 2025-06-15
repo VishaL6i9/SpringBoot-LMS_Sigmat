6f955cda56903985979826315028200edc5c91d4
VishaL6i9 <vishalkandakatla@gmail.com>
2025-06-15 18:49:17 +0530

    Added Repository For Notification Model

---
+++ src/main/java/com/sigmat/lms/repo/NotificationRepository.java
@@ -0,0 +1,18 @@
+package com.sigmat.lms.repo;
+
+import com.sigmat.lms.models.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUserIdOrderByTimestampDesc(Long userId);
+    List<Notification> findByUserIdAndIsReadFalseOrderByTimestampDesc(Long userId);
+    long countByUserIdAndIsReadFalse(Long userId);
+    long countByUserIdAndTimestampGreaterThanEqual(Long userId, Date date);
+
+    long countByUserId(Long userId);
+}
