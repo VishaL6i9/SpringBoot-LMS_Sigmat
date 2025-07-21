4d04440a773ea5487fd16188ce1af5ac1143405f
VishaL6i9 <vishalkandakatla@gmail.com>
2025-07-21 11:43:02 +0530

    fix: Adjust CourseRepo JPQL query for eager fetching
    Modified the `findByCourseIdWithModulesAndLessonsAndArticleContent` query in `CourseRepo.java`. The query now only eagerly fetches `Course` modules (`c.modules`) to resolve the `MultipleBagFetchException` caused by attempting to simultaneously fetch multiple `List` collections.

--- src/main/java/com/sigmat/lms/repo/CourseRepo.java
+++ src/main/java/com/sigmat/lms/repo/CourseRepo.java
@@ -2,11 +2,15 @@ package com.sigmat.lms.repo;
 
 import com.sigmat.lms.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CourseRepo extends JpaRepository<Course, Long> {
     Optional<Course> findByCourseId(Long courseId);
     Optional<Course> findByCourseCode(String courseCode);
-    
+
+    @Query("SELECT c FROM Course c LEFT JOIN FETCH c.modules m WHERE c.courseId = :courseId")
+    Optional<Course> findByCourseIdWithModulesAndLessonsAndArticleContent(@Param("courseId") Long courseId);
 }
