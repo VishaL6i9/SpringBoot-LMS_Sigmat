c9f98f7290975114b75a3e5b5b030c887d394df1
VishaL6i9 <vishalkandakatla@gmail.com>
2025-07-23 23:24:47 +0530

    feat: Implemented DTOs for webhook event handler

---
+++ src/main/java/com/sigmat/lms/dtos/WebhookEventDTO.java
@@ -0,0 +1,23 @@
+package com.sigmat.lms.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WebhookEventDTO {
+    private String sessionId;
+    private String subscriptionId;
+    private String invoiceId;
+    private Long userId;
+    private Long planId;
+    private Long courseId;
+    private Integer durationMonths;
+    private Double amount;
+    private String status;
+    private String reason;
+}
