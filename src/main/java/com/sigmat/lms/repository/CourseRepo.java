3c54e4f03700b0b66f628a9261cfa292272eb4d5
VishaL6i9 <vishalkandakatla@gmail.com>
2025-07-27 03:00:18 +0530

    feat: Added method to fetch courses related to instructor

--- src/main/java/com/sigmat/lms/repository/CourseRepo.java
+++ src/main/java/com/sigmat/lms/repository/CourseRepo.java
@@ -3,11 +3,13 @@ package com.sigmat.lms.repository;
 import com.sigmat.lms.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CourseRepo extends JpaRepository<Course, Long> {
     Optional<Course> findByCourseId(Long courseId);
     Optional<Course> findByCourseCode(String courseCode);
+    List<Course> findByInstructors_InstructorId(Long instructorId);
 
     // @Query("SELECT c FROM Course c LEFT JOIN FETCH c.modules m LEFT JOIN FETCH m.lessons l LEFT JOIN FETCH l.articleLesson al WHERE c.courseId = :courseId")
     // Optional<Course> findByCourseIdWithModulesAndLessonsAndArticleContent(@Param("courseId") Long courseId);
