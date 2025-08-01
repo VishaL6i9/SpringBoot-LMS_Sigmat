f6f3234366e79e08d44ef6e2c45a566ac09d65ba
VishaL6i9 <vishalkandakatla@gmail.com>
2025-08-01 19:15:32 +0530

    fix: Added missing JavaTimeModule

--- src/main/java/com/sigmat/lms/config/JacksonConfig.java
+++ src/main/java/com/sigmat/lms/config/JacksonConfig.java
@@ -2,6 +2,7 @@ package com.sigmat.lms.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -14,12 +15,12 @@ public class JacksonConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         
-        // Register Hibernate6Module to handle lazy loading
         Hibernate6Module hibernate6Module = new Hibernate6Module();
         hibernate6Module.disable(Hibernate6Module.Feature.USE_TRANSIENT_ANNOTATION);
         hibernate6Module.enable(Hibernate6Module.Feature.FORCE_LAZY_LOADING);
         
         mapper.registerModule(hibernate6Module);
+        mapper.registerModule(new JavaTimeModule());
         
         return mapper;
     }
